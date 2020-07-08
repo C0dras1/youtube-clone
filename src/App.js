d17cda5bd9bb1828ac8ab7aba6773828f2df04af
Jesse <jessesuderman@gmail.com>
2020-07-08 14:52:49 -0700

    VideoPreview Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import HeaderNav from './containers/HeaderNav/HeaderNav';
 import {SideBar} from './containers/SideBar/SideBar';
+import {Home} from './containers/Home/Home';
+import './containers/Home/Home.scss';
 
 
 class App extends Component {
@@ -9,6 +11,7 @@ class App extends Component {
       <React.Fragment>
         <HeaderNav />
         <SideBar />
+        <Home />
       </React.Fragment>
     )
   }
