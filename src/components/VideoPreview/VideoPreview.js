c163ac61fb3b743b4a0a3d88ee8354a1c32b9162
Jesse <jessesuderman@gmail.com>
2020-07-08 20:44:42 -0700

    VideoMetaData

--- src/components/VideoPreview/VideoPreview.js
+++ src/components/VideoPreview/VideoPreview.js
@@ -4,15 +4,16 @@ import './VideoPreview.scss';
 
 export class VideoPreview extends React.Component {
   render() {
+    const horizontal = this.props.horizontal ? 'horizontal' : null;
     return (
-      <div className='video-preview'>
+      <div className={['video-preview', horizontal].join(' ')}>
         <div className='image-container'>
           <Image src='http://via.placeholder.com/210x118' />
           <div className='time-label'>
             <span>05:22</span>
           </div>
         </div>
-        
+
         <div className='video-info'>
           <div className='semi-bold show-max-two-lines'>Video title</div>
           <div className='video-preview-metadata-container'>
