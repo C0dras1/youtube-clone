1559db6365e1254ac987264ef7c84ad5266f3cd5
Jesse <jessesuderman@gmail.com>
2020-07-09 07:03:22 -0700

    redux and redux-saga config

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import {BrowserRouter as Router} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from './store/configureStore';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
